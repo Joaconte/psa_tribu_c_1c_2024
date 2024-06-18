be67af781ca7ae576f3df4b9f2d21d82da68926a
Joaquin Conte Grand <43884898+Joaconte@users.noreply.github.com>
2024-06-18 19:40:58 -0300

    remove interface in types

--- components/types.ts
+++ components/types.ts
@@ -1,13 +1,5 @@
-import {Usuario} from "../types/types";
-
 export interface ISidebarItem {
   href: string
   title: string
   children?: ISidebarItem[]
-}
-
-export interface ModalProps {
-  modalOpen: boolean
-  setModalOpen: (x: boolean) => void
-  list: Usuario[]
 }
