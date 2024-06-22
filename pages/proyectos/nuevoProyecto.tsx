0e1042656028a7674073229f1ff03f9ffa377f11
Joaquin Conte Grand <43884898+Joaconte@users.noreply.github.com>
2024-06-22 15:10:37 -0300

    https

--- pages/proyectos/nuevoProyecto.tsx
+++ pages/proyectos/nuevoProyecto.tsx
@@ -24,7 +24,7 @@ export default function NuevoProyecto() {
 
   useEffect(() => {
 
-    const url = "http://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos"
+    const url = "https://anypoint.mulesoft.com/mocking/api/v1/sources/exchange/assets/754f50e8-20d8-4223-bbdc-56d50131d0ae/recursos-psa/1.0.0/m/api/recursos"
 
     const fetchResource = async () => {
 
